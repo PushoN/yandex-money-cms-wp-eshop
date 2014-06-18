ab8845181e15d0307c085aec10300690037eebea
DigitalCommunity <ant1freezeca@gmail.com>
2014-06-18 13:29:54 +0400

    cms_name added

--- views/form_protocol.php
+++ views/form_protocol.php
@@ -10,6 +10,7 @@
 <input type="hidden" name="shopSuccessURL" value="<?php echo get_permalink(get_option('eshop_success_page')); ?>" />
 <input type="hidden" name="shopFailURL" value="<?php echo get_permalink(get_option('eshop_fail_page')); ?>" />
 <input type="hidden" name="paymentType" value="<?php echo $paymentType; ?>" />
+<input type="hidden" name="cms_name" value="wordpress_eshop" />
 <input type="submit" value="<?php _e('Click to continue', 'eshop'); ?>" />
 </form>
 <script>
